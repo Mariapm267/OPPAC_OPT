c22872d7a6aa8ce861bd3e1af7f6dbe303be53e3
María Pereira <107856843+Mariapm267@users.noreply.github.com>
2024-04-23 16:36:33 +0200

    Update regressor.py

--- regressor.py
+++ regressor.py
@@ -12,7 +12,7 @@ class NeuralNetwork(nn.Module):
             layers.append(nn.Linear(input_size if i == 0 else hidden_size, hidden_size))
             #layers.append(nn.BatchNorm1d(hidden_size))  # batch normalization
             layers.append(nn.ReLU())
-            #layers.append(nn.Dropout(p=dropout))  # dropout
+            layers.append(nn.Dropout(p=dropout))  # dropout
             
             
         layers.append(nn.Linear(hidden_size, output_size))
@@ -25,47 +25,59 @@ class NeuralNetwork(nn.Module):
 
 
 
-def train_loop(dataloader, model, loss_fn, optimizer, scheduler):
+def train_loop(dataloader, model, loss_fn, optimizer, scheduler, use_tqdm=False):
     losses = []
     model.train()   # (training mode)
-    with tqdm(total=len(dataloader), desc='Training', unit='batch') as pbar:
-        for i, (y, X) in enumerate(dataloader):   # for each batch
-            
-            optimizer.zero_grad()
-            
-            # Forward pass
-            outputs = model(X)
-            loss = loss_fn(outputs, y)
+    
+    if use_tqdm:
+        pbar = tqdm(total=len(dataloader), desc='Training', unit='batch')
+    
+    for i, (y, X) in enumerate(dataloader):   # for each batch
+        optimizer.zero_grad()
+        
+        # Forward pass
+        outputs = model(X)
+        loss = loss_fn(outputs, y)
 
-            # Backpropagation
-            loss.backward()
-            optimizer.step()
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
 
-            losses.append(loss.item())
-            
+        losses.append(loss.item())
+        
+        if use_tqdm:
             pbar.set_postfix({'Loss': loss.item()})
             pbar.update(1)
             
+    if use_tqdm:
+        pbar.close()
+        
     scheduler.step()     # scheduler update at each epoch
     return np.mean(losses)
 
 
-
-
-def validation_loop(dataloader, model, loss_fn):
+def validation_loop(dataloader, model, loss_fn, use_tqdm=False):
     losses = []
     model.eval()     # (evaluation mode) 
     
-    with tqdm(total=len(dataloader), desc='Validation', unit='batch') as pbar:
-        with torch.no_grad():
-            for y, X in dataloader:
-                pred = model(X)
-                loss = loss_fn(pred, y).item()
-                losses.append(loss)
+    if use_tqdm:
+        pbar = tqdm(total=len(dataloader), desc='Validation', unit='batch')
+    
+    with torch.no_grad():
+        for y, X in dataloader:
+            pred = model(X)
+            loss = loss_fn(pred, y).item()
+            losses.append(loss)
+            if use_tqdm:
                 pbar.set_postfix({'Loss': loss})
                 pbar.update(1)
                 
+    if use_tqdm:
+        pbar.close()
+        
     return np.mean(losses)
+
+
 
 
 
