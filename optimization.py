a81039055c8d2222d1f32d5f4e400e86da235b6b
María Pereira <107856843+Mariapm267@users.noreply.github.com>
2024-06-18 19:54:17 +0200

    Added AbsVolumeWrapper

--- optimization.py
+++ optimization.py
@@ -29,5 +29,71 @@ Stated changes: adaption of FitParams to pass type-checking, heavy adaptation of
 
 
 
+    
+class AbsVolumeWrapper(Volume):
+    def __init__(self, initial_pressure, initial_collimator_length, alpha_batch, model,  lr=0.01, epochs=10000, device=DEVICE):
+        super().__init__(initial_pressure, initial_collimator_length, alpha_batch, device=device)
+        self.model = model
+        self.alpha_batch = alpha_batch.to(device)
+        self.pressure = initial_pressure.clone().requires_grad_(True).to(device)
+        self.collimator_length = initial_collimator_length.clone().requires_grad_(True).to(device)
+        self.trainable_inputs = torch.stack([self.collimator_length, self.pressure])
+        self.optimizer = optim.Adam([self.collimator_length, self.pressure], lr=lr)
+        self.loss_fn = nn.MSELoss()
+        self.epochs = epochs
+
+    def fit(self):
+        loss_values = []
+        p_values = []
+        d_values = []
+        for epoch in tqdm(range(self.epochs), desc="Searching for best parameters...", unit="epoch"):
+            self.optimizer.zero_grad()
+            
+            expanded_trainable_inputs = torch.stack([self.pressure, self.collimator_length]).expand(len(self.alpha_batch), -1)
+            inputs = torch.cat((expanded_trainable_inputs, self.alpha_batch), dim=1)
+            
+            predictions = self.model(inputs)
+            loss = self.loss_fn(predictions, self.alpha_batch[:, -2:])
+            
+            loss.backward()
+            self.optimizer.step()
+            
+            self.clamp_parameters()
+            loss_values.append(loss.item())
+            p_values.append(self.pressure.detach().cpu().item())
+            d_values.append(self.collimator_length.detach().cpu().item())
+
+        print(f'Best parameters are...\nPressure: {self.pressure.detach()} Torr\nCollimator length: {self.collimator_length.detach()} mm')
+        return loss_values, p_values, d_values
+
+    def plot_loss(self, loss_values, filename = 'loss_curve_OPT'):
+        plt.figure()
+        plt.plot(range(self.epochs), np.sqrt(loss_values) / 1000, label='Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss [cm]')
+        plt.title('Optimisation Loss Function')
+        plt.legend()
+        plt.savefig(f'../../figs/{filename}.pdf', dpi=400)
+        
+    def plot_pressure(self, p_values, filename = 'pressure_curve_OPT'):
+        plt.figure()
+        plt.plot(range(self.epochs), p_values)
+        plt.xlabel('Epoch')
+        plt.ylabel('p [Torr]')
+        plt.title('Evolution of pressure')
+        plt.savefig(f'../../figs/{filename}.pdf', dpi=400)
+        
+    def plot_col_lenght(self, d_values, filename = 'd_curve_OPT'):
+        plt.figure()
+        plt.plot(range(self.epochs), d_values)
+        plt.xlabel('Epoch')
+        plt.ylabel('d [mm]')
+        plt.title('Evolution of collimator length')
+        plt.savefig(f'../../figs/{filename}.pdf', dpi=400)
+        
+    
+
+
+
 
 # Lots of work to be done here
