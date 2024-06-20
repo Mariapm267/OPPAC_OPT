728b506befaccb5a28d64a5e45e2c723f3cec25b
María Pereira <107856843+Mariapm267@users.noreply.github.com>
2024-06-20 18:53:52 +0200

    Update run_opt.py

--- examples/run_opt.py
+++ examples/run_opt.py
@@ -1,30 +1,33 @@
 import torch
 import sys
+import random
+import matplotlib.pyplot as plt
+
 
 # From repo
 sys.path.append('../')
 import passive_generator 
 import optimization
 from core import DEVICE
+from alpha_batch import AlphaBatch
 
 # load pre-trained reconstruction model in eval mode
-
 model_path = '../models/Model2.pt'
 model= torch.load(model_path)                  
 model.eval()
 
 # use generator to generate a batch of beam possitions
-batch_size = 100000
+batch_size = 10000
 generator = passive_generator.RandomBeamPositionGenerator()
-alpha_batch = generator.generate_set(batch_size) 
+alpha_batch = AlphaBatch(generator.generate_set(batch_size))
 
-# set initial values of pressure and collimator length
-initial_pressure = torch.tensor(30.0, dtype=torch.float32)
-initial_collimator_length = torch.tensor(10.0, dtype=torch.float32)
+# fit
+pressure = torch.tensor(random.uniform(10,50), dtype=torch.float32)
+collimator_length = torch.tensor(random.uniform(5,50), dtype=torch.float32)
 
-
-VolumeWrapper = optimization.VolumeOptimizer(initial_pressure, initial_collimator_length, alpha_batch, model,  lr=0.1, epochs=1000, device=DEVICE)
+VolumeWrapper = optimization.AbsVolumeWrapper(pressure, collimator_length, alpha_batch, model,  lr=0.1, epochs=1000,  device=DEVICE)
 loss_values, p_values, d_values = VolumeWrapper.fit()
+
 
 plot = True
 if plot:
