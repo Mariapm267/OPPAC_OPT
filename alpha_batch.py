119b0cdef50c39493c6c1a14f32b3b0cb4340cd3
Mariapm267 <mariapm267@gmail.com>
2024-07-17 19:33:17 +0200

    Update alpha_batch.py

--- alpha_batch.py
+++ alpha_batch.py
@@ -3,23 +3,20 @@ import torch
 from torch import Tensor
 
 class AlphaBatch:
-  def __init__(self, xy_e: Tensor, device: torch.device = DEVICE):
-    self.device = device
-    self._alphas = xy_e.to(self.device)
-    self.beam_xy = xy_e
-    # distribuciones de fotones generadas por la detección del alpha
-    #self._photonDistribs: Dict[str, Dict[str, List[Tensor]]] = defaultdict(lambda: defaultdict(list))
+    def __init__(self, xy_e: Tensor, device: torch.device = torch.device('cpu')):
+        self.device = device
+        self._alphas = xy_e.to(self.device)
+        self.beam_xy = xy_e
 
+        def __repr__(self) -> str:
+            return f"Batch of {len(self)} alphas"
 
-    def __repr__(self) -> str:
-        return f"Batch of {len(self)} alphas"
-
-    def __len__(self) -> int:
-        return len(self._alphas)
+        def __len__(self) -> int:
+            return len(self._alphas)
+    
+    def get_beam_xy(self) -> Tensor:
+        return self.beam_xy
       
-  def get_photon_distribution(self) -> Tensor:
+    def get_photon_distribution(self) -> Tensor:
       '''Not implemented for now, as the reconstruction process is made separately'''
       pass
-
-    # analog of hits would be the photon distributions ( self.append_photonDistribs...)
-
