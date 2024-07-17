9745bddc20635fe9a8847e02eb05c71c75f0db8e
Mariapm267 <mariapm267@gmail.com>
2024-07-17 20:37:27 +0200

    Solved fig bugs

--- optimization.py
+++ optimization.py
@@ -7,7 +7,6 @@ import torch.nn as nn
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 
 # from this repo
 from volume import Volume
@@ -88,7 +87,7 @@ class AbsVolumeWrapper(Volume):
         print(f'Best parameters are...\nPressure: {self.pressure.detach()} Torr\nCollimator length: {self.collimator_length.detach()} mm')
         return loss_values, p_values, d_values
 
-    def plot_loss(self, loss_values, path = '../../figs/loss_curve_OPT.pdf', fontsize = 14):
+    def plot_loss(self, loss_values, path = '../figs/loss_curve_OPT.pdf', fontsize = 14):
         plt.figure()
         plt.plot(range(self.epochs), np.sqrt(loss_values) / 1000, label='Loss')
         plt.xlabel('Epoch', fontsize = fontsize)
@@ -97,7 +96,7 @@ class AbsVolumeWrapper(Volume):
         plt.legend()
         plt.savefig(path, dpi=400)
 
-    def plot_pressure(self, p_values, path = '../../figs/pressure_curve_OPT.pdf', additional_text = None):
+    def plot_pressure(self, p_values, path = '../figs/pressure_curve_OPT.pdf', additional_text = None):
         plt.figure()
         plt.plot(range(len(p_values)), p_values)
         plt.xlabel('Epoch')
@@ -105,7 +104,7 @@ class AbsVolumeWrapper(Volume):
         plt.title(f'Evolution of pressure {additional_text}')
         plt.savefig(path, dpi=400)
 
-    def plot_col_lenght(self, d_values , path = '../../figs/pressure_curve_OPT.pdf', fontsize = 14):
+    def plot_col_length(self, d_values , path = '../figs/length_curve_OPT.pdf', fontsize = 14):
         plt.figure()
         plt.plot(range(len(d_values)), d_values)
         plt.xlabel('Epoch')
