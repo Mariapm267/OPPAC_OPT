b452023f93354f196a132ca647a0fc246d5e1401
María Pereira <107856843+Mariapm267@users.noreply.github.com>
2024-09-11 19:06:19 +0200

    Update run_opt.py

--- examples/run_opt.py
+++ examples/run_opt.py
@@ -1,29 +1,33 @@
 import torch
+
+# Basic stuff
 import sys
 import random
 import matplotlib.pyplot as plt
 import os
 
-# From repo
+# From this repo
 sys.path.append('../')
 import optimization
 from core import DEVICE
 from alpha_batch import AlphaBatch
 
 # load pre-trained reconstruction model in eval mode
-model_path = '../models/Model2.pt'
+model_path = '../models/Model.pt'
 model= torch.load(model_path)                  
 model.eval()
 
-# generate an alpha batch
+# generate an alpha batch (positions of random impining particles)
 batch_size = 10000
 alpha_batch = AlphaBatch(batch_size=batch_size)
 
-# fit
+# Initial values of pressure and collimator length
 pressure = torch.tensor(random.uniform(10,50), dtype=torch.float32)
 collimator_length = torch.tensor(random.uniform(5,50), dtype=torch.float32)
 
 VolumeWrapper = optimization.AbsVolumeWrapper(pressure, collimator_length, alpha_batch, model,  lr=0.1, epochs=1000,  device=DEVICE)
+
+# fit
 loss_values, p_values, d_values = VolumeWrapper.fit()
 
 
